ebe9cd413deb9e032a26f692b3da451c01b64a63
kimbs95 <lifee456@naver.com>
2022-01-19 17:07:54 +0900

    #첫날

---
+++ 입출력_사칙연산.js
@@ -0,0 +1,49 @@
+// 1
+console.log('Hello World!');
+// 2
+console.log('강한친구 대한육군');
+console.log('강한친구 대한육군');
+// 3
+const abc = [2,3]; 
+let a = abc[0];
+let b = abc[1];
+const result = a+b;
+console.log(result);
+// 4
+const results = a-b;
+console.log(results);
+// 5
+const multi = a*b;
+console.log(multi);
+// 6
+const divide = a/b;
+console.log(divide);
+// 7
+let lists = {
+    ejgkrl:a+b,
+    Qorl:a-b,
+    rhqgkrl:a*b,
+    sksnrl:a/b,
+    skajwl:a%b
+
+};
+console.log(lists);
+// 8
+const number = [5,8,4];
+
+let d = number[0];
+let e = number[1];
+let f = number[2];
+    
+let resolve = {
+    first:(d+e)%f,
+    second:((d%f)+(e%f))%f,
+    multis:(d*e)%f,
+    net:((d%f)*(e%f))%f
+};
+console.log(resolve);
+console.log(resolve.net);
+
+
+
+
