8eeb8895594d556f5604f31b82d5a916a49f040b
kimbs95 <lifee456@naver.com>
2022-01-25 16:36:09 +0900

    [22.01.25]#백준문제

--- 3.for문.js
+++ 3.for문.js
@@ -52,3 +52,45 @@ for(i=0;i<leng;i++){
     let num4 = kk[i][1]; 
     console.log(num3+num4 +8);
 }
+// 9
+console.log('===============');
+let ac=5;
+let star = '';
+let blank='';
+for(let i= 1; i<=ac; i++){
+    star += '*'; //  true 일때 매순간순간 star 를 한번씩 찍고  위에 저장이된다.
+    for(let e =0; e < ac-i; e++){
+        blank += ' ';// 다음 for 문으로 들어와 for 문이 끝날때까지 돌아간다.
+    }
+    console.log(blank + star);// 출력을 하여 보여준후 
+    blank='';// 초기화를 해줘야 전역 변수에서도 초기화가 된다.
+}
+console.log('@@@@@@@@@@@@@@@@@@@');
+// 배열 선언하는 방법 2)
+let x = 5;
+
+let art= new Array(x).fill(' ');
+// new Array 를 사용하여 x 만큼의 인덱스를 만듬
+// fill 메서드는 배열 시작부터 끝까지 정적인 값 하나를 채움
+
+for(let a = x-1; a > 0; a--){
+    art[a]='*'; 
+    console.log(art.join(''));
+    //join  메서드는 배열 모두를 연결하여 하나로만듬
+}
+console.log('@@@@@@@@@@@@@@@@@@');
+
+// 10
+let ww = 10; 
+let yy = 5; 
+let tt = '';
+
+let arr1 =[1,10,4,9,2,3,8,5,7,6];
+for(let i of arr1){
+    if(yy>i){
+        tt += `${i} `;
+    }
+}
+console.log(tt);
+
+console.log('============');
